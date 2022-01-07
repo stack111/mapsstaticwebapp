c1ced82f603663c25a4e63eabd4755b624e4978a
Daniel Stack <dstack@microsoft.com>
2022-01-06 18:01:15 -0800

    check in working sample

--- src/api/MapsFunc.cs
+++ src/api/MapsFunc.cs
@@ -17,7 +17,7 @@ namespace Azure.Maps
             log.LogInformation("C# HTTP trigger function processed a request.");
             dynamic response = new 
             {
-                sasToken = "<sastoken>"
+                sasToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWM4Njg0OS1lYzUyLWVlYmItNzc5MC0xNWQwOTM5ZDY5ODkiLCJyZWdpb25zIjoiW10iLCJtYXhSYXRlUGVyU2Vjb25kIjoiNTAwIiwib2lkIjoiMGY4NmE3ODEtYzdjMi00MjVmLWIyZGQtYmYyYTY2N2RlNmQ5IiwibmJmIjoxNjM1MTM2NTkxLCJleHAiOjE2NjY2NzI1OTEsImlhdCI6MTYzNTEzNjY0MSwiaXNzIjoiZTYzMzE2ZjMtYjQ1MC00MWNjLWFlZTQtYmM2NmU3OTU3ZTM0IiwiYXVkIjoiL3N1YnNjcmlwdGlvbnMvZDllZWY2NTAtYTg3Yy00MzNiLWEyMzYtMGVhZjMxNDBkOTIxL3Jlc291cmNlR3JvdXBzL21hcHMtZHN0YWNrLXRlbXBzYW1wbGUvcHJvdmlkZXJzL01pY3Jvc29mdC5NYXBzL2FjY291bnRzL21hcHMzbnVhYWpzaXpldnUifQ.t0ujrmoHNhrZxukCPR3tPuiuJQxv2VmI2uzxJoUzorE"
             };
             return new JsonResult(response);
         }
